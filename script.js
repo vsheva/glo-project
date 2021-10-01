6f774ac9c7c4b71c8c36290c537b9c9d9afc34dd
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-02 00:58:07 +0300

    Hard task for lesson02

--- script.js
+++ script.js
@@ -12,5 +12,24 @@ console.log(screens.length);
 console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
 console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
 console.log(screens.toLowerCase().split(" "));
-console.log(fullPrice * (rollback/100));
+console.log(fullPrice * (rollback / 100));
 
+
+// Complicated
+
+function mult(num) {
+    let stringNum = num.toString();
+    let total = 1;
+    for (let i = 0; i < stringNum.length; i++) {
+        total = total * stringNum[i];
+    }
+    let z = total ** 3;
+    let digit = (z).toString().split('').slice(0, 2);
+
+    console.log(total);
+    console.log(z);
+    console.log(digit.join(''));
+
+}
+
+mult(266219);
