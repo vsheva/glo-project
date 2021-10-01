0744f02216c423fd4f5090808a733f40e537e2bb
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-01 22:08:36 +0300

    Lesson 02 complete

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title = "project",
-  screens = "Simple, Complex, Interactive",
-  screenPrice = 5,
-  rollback = 65,
-  fullPrice = 100,
-  adaptive = true;
+let title = 'project',
+    screens = 'Simple, Complex, Interactive',
+    screenPrice = 21,
+    rollback = 50,
+    fullPrice = 100,
+    adaptive = true;
 
 console.log(title);
 console.log(fullPrice);
@@ -11,6 +11,6 @@ console.log(adaptive);
 console.log(screens.length);
 console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
 console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+console.log(screens.toLowerCase().split(" "));
+console.log(fullPrice * (rollback/100));
 
-
-alert("Hello world");
