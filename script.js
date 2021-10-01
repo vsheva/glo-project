e1d556419722ebddea4ceeacc2c7f135f082dc9a
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-01 20:59:33 +0300

    Add some changes

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-let title = "project",
-  screens = "Simple, Complex, Interactive",
-  screenPrice = 5,
-  rollback = 65,
-  fullPrice = 100,
-  adaptive = true;
+let title, screens, screenPrice, rollback, fullPrice, adaptive;
 
-console.log(title);
-console.log(fullPrice);
-console.log(adaptive);
-console.log(screens.length);
-console.log(
-  `Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`
-);
-console.log(
-  `Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`
-);
+console.log("Hello Bohdan!");
 
 alert("Hello world");
