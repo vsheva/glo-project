32274cf3095c0b3d015f410e675dbdc506d14a2d
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-01 20:45:55 +0300

    Start tasks

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = "project",
+  screens = "Simple, Complex, Interactive",
+  screenPrice = 5,
+  rollback = 65,
+  fullPrice = 100,
+  adaptive = true;
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+
 
 alert("Hello world");
