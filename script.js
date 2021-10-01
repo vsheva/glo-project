49f560747c3649c751a2b1b84680f59de8d15844
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-01 22:07:05 +0300

    Tasks for Lesson02

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+let title = 'project',
+    screens = 'Simple, Complex, Interactive',
+    screenPrice = 21,
+    rollback = 50,
+    fullPrice = 100,
+    adaptive = true;
 
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+console.log(screens.toLowerCase().split(" "));
+console.log(fullPrice * (rollback/100));
 
-console.log("Hello Bohdan!");
-
-alert("Hello world");
 
