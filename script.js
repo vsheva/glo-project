3a043ece8467bc81cf05c6ef8afa11fc03b07aa9
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-04 00:55:56 +0300

    Lesson02_hard refactroring

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-let title = 'project',
-    screens = 'Simple, Complex, Interactive',
-    screenPrice = 21,
-    rollback = 50,
-    fullPrice = 100,
-    adaptive = true;
-
-console.log(title);
-console.log(fullPrice);
-console.log(adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
-console.log(screens.toLowerCase().split(" "));
-console.log(fullPrice * (rollback / 100));
-
-
 // Complicated
 
 function mult(num) {
@@ -33,3 +16,12 @@ function mult(num) {
 }
 
 mult(266219);
+
+
+
+
+
+
+
+
+
