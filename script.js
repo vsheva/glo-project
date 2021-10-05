5b3dcf71dbbe5bfdfef18fd6ee1b16ea6f0d77a8
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-05 21:52:41 +0300

    Lesson04 solution

--- script.js
+++ script.js
@@ -3,37 +3,63 @@
 let title = prompt('Как называется ваш проект?'),
     screens = prompt('Какие типы экранов нужно разработать?'),
     screenPrice = +prompt('Сколько будет стоить данная работа?'),
-    rollback = 50,
-    adaptive = confirm('Нужен ли адаптив на сайте?');
-
-let service1 = prompt('Какой дополнительный тип услуги нужен?'),
+    adaptive = confirm('Нужен ли адаптив на сайте?'),
+    service1 = prompt('Какой дополнительный тип услуги нужен?'),
     servicePrice1 = +prompt('Сколько это будет стоить?'),
     service2 = prompt('Какой дополнительный тип услуги нужен?'),
-    servicePrice2 = +prompt('Сколько это будет стоить?');
+    servicePrice2 = +prompt('Сколько это будет стоить?'),
+    rollback = 10;
+    let fullPrice = screenPrice + servicePrice1 + servicePrice2;
 
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * rollback / 100));
 
+let allServicePrices = function () {
+    return servicePrice1 + servicePrice2;
+}
 
-if (fullPrice >= 30000) {
-    console.log('скидка 10%');
-} else if (fullPrice >= 15000 && fullPrice < 30000) {
-    console.log('скидка 5%');
-} else if (fullPrice >= 0 && fullPrice < 15000) {
-    console.log('Скидка не предусмотрена');
-} else if (fullPrice < 0) {
-    console.log('Что то пошло не так');
+function getFullPrice() {
+    let fullPrice = screenPrice + allServicePrices;
+    return fullPrice;
+}
+
+function getTitle() {
+    if (!title) return title;
+    return title[0].toUpperCase() + title.slice(1);
+}
+
+let servicePercentPrice = function () {
+    return servicePercentPrice = Math.ceil(fullPrice - (fullPrice * rollback / 100));
 }
 
 
-console.log(title);
-console.log(fullPrice);
-console.log(adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} долларов`);
-console.log(`Стоимость разработки сайта ${fullPrice} долларов`);
+let showTypeOf = function (variable) {
+    console.log(variable, typeof variable);
+}
+
+
+const getRollbackMessage = function (price) {
+    if (price >= 30000) {
+        return 'Даем скидку в 10%';
+    } else if (price >= 15000 && price < 30000) {
+        return 'Даем скидку в 5%';
+    } else if (price >= 0 && price < 15000) {
+        return 'Скидка не предусмотрена';
+    } else if (price < 0) {
+        return 'Что то пошло не так';
+    }
+}
+
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(getRollbackMessage(fullPrice));
 console.log(screens.toLowerCase().split(" "));
-console.log(servicePercentPrice);
+console.log(servicePercentPrice());
+
+
+
+
 
 
 
