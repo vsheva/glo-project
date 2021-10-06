4ba76cec231e8c4eb7fcce1405527289bf9793f4
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-06 14:56:05 +0300

    Refactoring

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 'use strict'
 
-let lang;
 
-if (lang = "ru") {
+let lang = "ru";
+
+if (lang === "ru") {
     console.log('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье');
-} else if (lang = "en") {
+} else if (lang === "en") {
     console.log('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
 }
-
 
 switch (lang) {
     case "ru":
@@ -18,13 +18,14 @@ switch (lang) {
         break;
 }
 
-
 let  wdays = {
 {'ru': 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'},
 {'en':'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'},
 };
 console.log(wdays['ru']);
 console.log (wdays['en']);
+
+
 
 
 let namePerson;
