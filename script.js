875cd3b33e6343091189df55dca9dee0e1c1bb75
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-13 17:53:36 +0300

    Refactoring

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 'use strict'
+//
+// let arr = ['25', '44', '20', '5', '0', '777', '2000'];
+//
+// arr.forEach(item => {
+//     if (item.startsWith('2') || item.startsWith('4')) {
+//         console.log(item);
+//     }
+// });
+//
+// function isPrimeNumber(n) {
+//     for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
+//     return n > 1;
+// }
+//
+// const res = [...Array(41)].reduce((a, _, i) => a.concat(isPrimeNumber(i) ? `Делители числа ${i}: 1 и ${i}` : []), []).join('\n');
+//
+//
 
-let arr = ['25', '44', '20', '5', '0', '777', '2000'];
-
-arr.forEach(item => {
-    if (item.startsWith('2') || item.startsWith('4')) {
-        console.log(item);
+let c = 4
+function addX(x) {
+    return function(n) {
+        return n + x
     }
-});
-
-function isPrimeNumber(n) {
-    for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
-    return n > 1;
 }
-
-const res = [...Array(41)].reduce((a, _, i) => a.concat(isPrimeNumber(i) ? `Делители числа ${i}: 1 и ${i}` : []) , []).join('\n');
-
-
-
-
+const addThree = addX(3)
+let d = addThree(c)
+console.log('example partial application', d)
