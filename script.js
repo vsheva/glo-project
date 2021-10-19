e5fd30930d57eb3eaf5eee47c9082180f484d61d
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-20 00:38:53 +0300

    Complete lesson05_hard01

--- script.js
+++ script.js
@@ -1,28 +1,18 @@
 'use strict'
-//
-// let arr = ['25', '44', '20', '5', '0', '777', '2000'];
-//
-// arr.forEach(item => {
-//     if (item.startsWith('2') || item.startsWith('4')) {
-//         console.log(item);
-//     }
-// });
-//
-// function isPrimeNumber(n) {
-//     for (let i = 2; i * i <= n; i == 2 ? i++ : i += 2) if (n % i == 0) return false;
-//     return n > 1;
-// }
-//
-// const res = [...Array(41)].reduce((a, _, i) => a.concat(isPrimeNumber(i) ? `Делители числа ${i}: 1 и ${i}` : []), []).join('\n');
-//
-//
+let userInput;
 
-let c = 4
-function addX(x) {
-    return function(n) {
-        return n + x
-    }
+
+function isNullOrWhiteSpace(n) {
+    return (
+        n === undefined || n === null || n.match(/^\s+|\s+$/) !== null || isNaN(n)
+    );
 }
-const addThree = addX(3)
-let d = addThree(c)
-console.log('example partial application', d)
+
+function numTrim(n) {
+    do {
+        n = prompt("Введите аргумент");
+    } while (isNullOrWhiteSpace(n));
+    return n.trim();
+}
+
+console.log(numTrim(userInput));
