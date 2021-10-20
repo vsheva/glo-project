1a32c01ceef3c208a1c72eef25b9416f71bac37c
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-20 16:34:21 +0300

    Lesson05_hard01 complete

--- script.js
+++ script.js
@@ -1,50 +1,19 @@
 'use strict'
 
-let screenPrice = prompt('Сколько будет стоить данная работа?'
-
-let isNumber = function (num) {
-    if (!isNaN(parseFloat(num)) && isFinite(num) && (num.trim() != '')) {
-        return number;
-    }
-};
-
-const asking = function () {
-    if (!isNumber(screenPrice)) {
-       return screenPrice = prompt('Сколько будет стоить данная работа?');
-    }
-};
-
-asking();
-let number =isNumber(screenPrice);
-console.log(number);
+let userInput;
 
 
+function isNullOrWhiteSpace(n) {
+    return (
+        n === undefined || n === null || n.match(/^\s+|\s+$/) !== null || isNaN(n)
+    );
+}
 
+function numTrim(n) {
+    do {
+        n = prompt("Введите аргумент");
+    } while (isNullOrWhiteSpace(n));
+    return n.trim();
+}
 
-
-
-
-let number1 = function (num) {
-    if (isNumber(num)) {
-        return Number(num);
-    }
-    return 0;
-else
-    if (!isNaN(parseFloat(num)) && isFinite(num)) {
-        return Number(num);
-    }
-};
-
-const asking = function () {
-    let price = prompt('Сколько это будет стоить?');
-
-    while (!isNumber(price)) {
-        price = prompt('Сколько это будет стоить?');
-    }
-};
-
-
-asking();
-
-let number = number1(price);
-console.log(number);
+console.log(numTrim(userInput));
