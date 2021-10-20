9778c4003b1312bfed9d55bb85c8e7cf2b58519b
Valera Shevchenko <valera.sheva@gmail.com>
2021-10-20 17:07:04 +0300

    Complete

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 'use strict'
 
-let userInput;
+let arr = ["2", "33", "455", "88", "99", "100", "25"];
 
+arr.forEach( function(item) {
+    if (item.startsWith('2') || item.startsWith('4')) {
+        console.log(item);
+    }
+});
 
-function isNullOrWhiteSpace(n) {
-    return (
-        n === undefined || n === null || n.match(/^\s+|\s+$/) !== null || isNaN(n)
-    );
+for (let i = 2; i < 100; i++) {
+    let count = 0;
+    for (let j = 2; j <= i && count < 2; j++) {
+        if (i % j == 0) {
+            count++;
+        }
+    }
+    if (count < 2)
+        console.log(i);
 }
-
-function numTrim(n) {
-    do {
-        n = prompt("Введите аргумент");
-    } while (isNullOrWhiteSpace(n));
-    return n.trim();
-}
-
-console.log(numTrim(userInput));
